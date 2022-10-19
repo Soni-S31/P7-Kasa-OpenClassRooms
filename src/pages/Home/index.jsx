5c06e0fcb6231e83a3e02c7c22256a88eede106f
sonia santos <santossonia31@gmail.com>
2022-10-19 09:26:53 +0200

    banner padding

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import BannerHome from '../../assets/bannerHome.png'
-//import BannerHomeMobile from '../../assets/bannerHomeMobile.png'
 import styled from 'styled-components'
+import colors from '../../utils/style/colors'
 
-
+const Main = styled.main`
+    padding: 0px 100px;
+    @media (max-width: 899px){
+      padding : 0px 20px;
+    }`
+    
 const Banner = styled.div`
     display:flex;
     position:relative;
     background: rgba(0,0,0,.5);
     border-radius: 25px;
-    width: 90%;
-    height: 100%;
-    margin: auto;
+    
     align-items: center;
     justify-content: center;
+    
     @media (max-width: 899px){
     height:110px;
     border-radius: 10px;
@@ -25,6 +29,7 @@ const ImgTitle = styled.img`
     margin: auto;
     border-radius: 25px;
     opacity: 0.6;
+    
     @media (max-width: 899px){
     height:110px;
     border-radius: 10px;
@@ -49,17 +54,25 @@ const TitleHome = styled.h2`
     right:25%;
     }`
 
+    const Location = styled.article`
+      background: ${colors.secondary};
+      display: flex;
+      
+      `
 function Home() {
   return (
-    <main>
+    <Main>
         <Banner>
           <ImgTitle src={BannerHome} alt="Chez vous, partout et ailleurs" />
           <TitleHome>Chez vous, partout et ailleurs</TitleHome>
-          </Banner>
+        </Banner>
+        <Location>
+
+        </Location>
       Page d'accueil hello
         
      
-    </main>
+    </Main>
   );}
 
 export default Home;
