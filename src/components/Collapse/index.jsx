ba5a9873c36330c4ee7c429cc759d20f379184f8
sonia santos <santossonia31@gmail.com>
2022-10-24 14:22:32 +0200

    collapse deroulé ou pas

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+//import Chevron from './chevron';
 import './collapse.css'
 
 
 export default function Collapse (props) {
+  const [setActive, setActiveState] = useState("");
+  const [setHeight, setHeightState] = useState("0px");
+  
+  const content = useRef(null);
+  
+  function toggleCollapse() {
+    setActiveState(setActive === "" ? "active" : "");
+    setHeightState(
+      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+    );
     
+  }
   return (
         <div className="collapse_div">
-          <button className="collapse_btn"  >
+          <button className={`collapse_btn ${setActive}`} onClick={toggleCollapse}  >
             <p className="collapse_title" >{props.title}</p>
+           
+      </button>
     
-          </button>
-          <div className="collapse_content">
+          <div className="collapse_content" 
+          ref={content}
+          style={{ maxHeight: `${setHeight}` }}>
             <p className="collapse_text">{props.content}</p>
           </div>
         </div>
